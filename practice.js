fcf02d041c3ef32cec84680a1d58cb57cdb43afa
Armando Roman <armando.roman.jr20@gmail.com>
2020-09-30 21:47:57 -0500

    pushing up more javascript problems

--- practice.js
+++ practice.js
@@ -8,19 +8,43 @@
     // Output: [0,1]
     // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-    var twoSum = function(nums, target){
-        var result = [];
+    // var twoSum = function(nums, target){
+    //     var result = [];
+    //
+    //     for (var i = 0; i < nums.length; i++) {
+    //         for (var j = i + 1; j < nums.length; j++) {
+    //             if (nums[i] + nums[j] === target) {
+    //                 result.push(i);
+    //                 result.push(j);
+    //             }
+    //         }
+    //     }
+    //     return result;
+    // }
+    // console.log(twoSum([2, 7, 11, 15], 9));
 
-        for (var i = 0; i < nums.length; i++) {
-            for (var j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] === target) {
-                    result.push(i);
-                    result.push(j);
-                }
-            }
-        }
-        return result;
-    }
-    console.log(twoSum([2, 7, 11, 15], 9));
 
+// Given a 32-bit signed integer, reverse digits of an integer.
 
+    //Example 1:
+    //
+    // Input: 123
+    // Output: 321
+    // Example 2:
+    //
+    // Input: -123
+    // Output: -321
+
+    var reverse = function(x) {
+        return (
+            parseFloat(
+                x
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+            ) * Math.sign(x)
+        )
+    };
+
+    console.log(reverse(-543.21))

a184f094456e99269d4babbb850859444e1eb05e
Armando Roman <armando.roman.jr20@gmail.com>
2020-10-02 12:48:57 -0500

    pushing

--- practice.js
+++ practice.js
@@ -148,3 +148,20 @@
     //
     // console.log(romanToInt("VII"))
 
+
+    // Given an array of integers, find the sum of its elements.
+
+        // For example, if the array ar = [1,2,3], 1 + 2 + 3 = 6 , so return 6.
+
+    // function simpleArraySum(ar) {
+    //     /*
+    //      * Write your code here.
+    //      */
+    //     let sum = 0;
+    //
+    //     for(var i = 0; i<ar.length; i++){
+    //         sum += ar[i];
+    //
+    //     }
+    //     return sum
+    // }

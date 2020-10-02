ed996b3f7bcfdddc2b638254bd507d979f6bcfdc
Armando Roman <armando.roman.jr20@gmail.com>
2020-10-01 23:58:15 -0500

    romanTointeger

--- practice.js
+++ practice.js
@@ -67,12 +67,21 @@
     // Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 
     // var isPalindrome = function(x) {
+    //      make sure # is not negative
     //     if (x < 0) return false
     //
-    //     let reversed = 0, y = x
+    //      setting reversed var
+    //     let reversed = 0
+
+    //      setting copy of x
+    //     let y = x
+    //
     //
     //     while (y > 0) {
+    //         we do modulus 10 to
     //         const lastDigit = y % 10
+
+    //            this is to append to the last digit
     //         reversed = (reversed * 10) + lastDigit
     //         y = (y / 10) | 0
     //     }
@@ -80,4 +89,62 @@
     // };
     //
     // console.log(isPalindrome(121))
+
+
+    // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    //
+    //     Symbol       Value
+    // I             1
+    // V             5
+    // X             10
+    // L             50
+    // C             100
+    // D             500
+    // M             1000
+    // For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+    //
+    // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+    //
+    //     I can be placed before V (5) and X (10) to make 4 and 9.
+    // X can be placed before L (50) and C (100) to make 40 and 90.
+    // C can be placed before D (500) and M (1000) to make 400 and 900.
+    // Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+    //
+    // Example 1:
+    //
+    // Input: "III"
+    // Output: 3
+    // Example 2:
+    //
+    // Input: "IV"
+    // Output: 4
+
+    //
+    // var romanToInt = function(s) {
+    //     let table = {
+    //         I: 1,
+    //         V: 5,
+    //         X: 10,
+    //         L: 50,
+    //         C: 100,
+    //         D: 500,
+    //         M: 1000
+    //     }
+    //
+    //
+    //     let result = 0;
+    //     for (let i = 0; i < s.length; i++) {
+    //         //if the next roman numeral is larger, then we know we have to subtract this number
+    //         if (table[s[i]] < table[s[i+1]]) {
+    //             result-=table[s[i]]
+    //         }
+    //         //otherwise, add like normal.
+    //         else {
+    //             result+=table[s[i]]
+    //         }
+    //     }
+    //     return result
+    // };
+    //
+    // console.log(romanToInt("VII"))
 
